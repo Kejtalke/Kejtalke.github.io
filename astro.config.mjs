fe0a375577f33a57172e30fb6487485161e4d6b4
Norbert Wlodarczyk <norbert.wlodarczyk@invia.com>
2026-03-02 16:17:01 +0100

    Add it to the github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+  site: 'https://strefa-zmian.pl',
   vite: { plugins: [tailwindcss()] },
 });
