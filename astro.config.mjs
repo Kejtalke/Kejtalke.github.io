dbb172ba8947bce626321605a9aac35c8832eae7
Norbert Wlodarczyk <norbert.wlodarczyk@invia.com>
2026-03-20 09:08:05 +0100

    Add sitemaps, change logo, and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
+import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://strefazmian-poznan.pl',
+  integrations: [sitemap()],
   vite: { plugins: [tailwindcss()] },
 });
